696ba5933346342746ead7f579826b451d20ca22
이재혁 <80149657+LeeJeaHyuk@users.noreply.github.com>
2022-08-26 14:11:51 +0900

    java jungsuk ch3~4 practice file upload

--- junsuk/src/ch3prac7.java
+++ junsuk/src/ch3prac7.java
@@ -1,5 +1,5 @@
 
-public class ex7 {
+public class ch3prac7 {
 		public static void main(String[] args) {
 		int fahrenheit = 100;
 
@@ -9,6 +9,8 @@ public class ex7 {
 		}
 }
 //'C = 5/9 ×(F - 32)
+// int를 사용해서 소수 2째 자리까지만 표현하는 방법을 사용해야 한다.
+
 /*
 37.77778
 37.78278 
--- junsuk/src/ch3prac9.java
+++ junsuk/src/ch3prac9.java
@@ -1,11 +1,12 @@
-
-public class ex9 {
+public class ch3prac9 {
 
 		public static void main(String[] args) {
 		char ch = 'z';
 		boolean b = ('A'<=ch && ch<='Z'||'a'<=ch && ch<='z'||'0'<=ch && ch<=9);
 		System.out.println(b);
 		}
-
-
 }
+
+// 중첩 삼항 연산자
+// 위와 같이 사용할 수 있지만 가독성이 좋지 않다.
+// if를 사용하면 가독성이 늘어난다.
