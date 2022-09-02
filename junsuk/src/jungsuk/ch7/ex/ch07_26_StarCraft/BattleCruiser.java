7d78af95edb988d4b3c838a2033343adfee8d05a
이재혁 <80149657+LeeJeaHyuk@users.noreply.github.com>
2022-09-02 10:38:20 +0900

    StarCraft파일 변경

---
+++ junsuk/src/jungsuk/ch7/ex/ch07_26_StarCraft/BattleCruiser.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package jungsuk.ch7.ex.ch07_26_StarCraft;
+
+class BattleCruiser extends AirUnit implements Repairable {
+
+	BattleCruiser() {
+		super(300);
+		hitPoint = MAX_HP;
+	}
+
+}
--- junsuk/src/jungsuk/ch7/ex/ch07_26_StarCraft/RepairableTest.java
+++ junsuk/src/jungsuk/ch7/ex/ch07_26_StarCraft/RepairableTest.java
@@ -14,7 +14,10 @@ class RepairableTest{
 	}
 }
 
-interface Repairable {}
+interface Repairable { } 
+// datatype으로써의 용도
+// 수리될 수 있는 타입이다
+
 class GroundUnit extends Unit {
 	GroundUnit(int hp) {
 		super(hp);
@@ -27,9 +30,12 @@ class AirUnit extends Unit {
 	}
 }
 
-class Unit {
+class Unit { 
+	//unit이 동시에 가지고 있는 것
+	// 체력, 최대체력
 	int hitPoint;
 	final int MAX_HP;
+	
 	Unit(int hp) {
 		MAX_HP = hp;
 	}
@@ -74,8 +80,8 @@ class SCV extends GroundUnit implements Repairable{
 		hitPoint = MAX_HP;
 	}
 
-	void repair(Repairable r) {
-		if (r instanceof Unit) {
+	void repair(Repairable r) { //수리가능한 타입
+		if (r instanceof Unit) { // r가 unit의 subclass인지
 			Unit u = (Unit)r;
 			while(u.hitPoint!=u.MAX_HP) {
 				/* Unit의 HP를 증가시킨다. */
@@ -84,5 +90,22 @@ class SCV extends GroundUnit implements Repairable{
 			System.out.println( u.toString() + "의 수리가 끝났습니다.");
 		}
 	}	
+	/*
+	 * 예를 들어 repairable하고  building인 객체가 추가된다면 
+	 * 위의 예시에서는 Unit이 최대 조상이지만(Object제외)
+	 * 전부 다합친 그 위의 조상을 만들고 Unit과 교차해주면 된다
+	 * ex ->Terran-(Unit, Building)-(GroundUnit,...)
+	 */
 	//...
 }
+/*
+class Barrack extends Buliding implements Liftable{
+	LiftableImpl l = new LiftableImpl();
+	
+	void liftOff() {
+		l.liftOff();
+	}
+}
+
+class LiftableImpl implements Liftable { }
+*/
